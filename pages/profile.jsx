48db82672bc6d65582a643a4f8848983c81bce06
yaroslav-kulpan <51779177+yaroslav-kulpan@users.noreply.github.com>
2021-05-13 02:51:12 +0300

    +++

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import Layout from "../components/common/Layout";
+import ProfileForm from "../components/ProfileForm";
 
 const Profile = () => {
     return (
         <Layout title="Profile">
-
+            <ProfileForm/>
         </Layout>
     );
 };
 
-export const getStaticProps = async ({ locale }) => ({
+export const getStaticProps = async ({locale}) => ({
     props: {
         ...await serverSideTranslations(locale, ['common', 'main']),
     },
