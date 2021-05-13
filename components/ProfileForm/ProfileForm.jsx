7b21e3266bf417a46db89fb989b650181b51ae26
yaroslav-kulpan <51779177+yaroslav-kulpan@users.noreply.github.com>
2021-05-13 03:23:57 +0300

    +++

--- components/ProfileForm/ProfileForm.jsx
+++ components/ProfileForm/ProfileForm.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Field, Form, Formik} from "formik";
+import {ErrorMessage, Field, Form, Formik} from "formik";
 import axios from "axios";
 import Cookie from 'js-cookie'
 import useLogout from "../../auth/hooks/use-logout";
@@ -55,12 +55,15 @@ const ProfileForm = () => {
         }} onSubmit={createOrder}>
             <Form>
                 <Field type="text" placeholder="Полное Имя Мужа" name="fullNameOfTheHusband"/>
+                <ErrorMessage name="fullNameOfTheHusband" />
                 <Field type="text" placeholder="Полное Имя Жены" name="WifeSFullName"/>
+                <ErrorMessage name="WifeSFullName" />
                 <Field type="text" placeholder="Кем выдан паспорт" name="issuedBy"/>
                 <Field type="text" placeholder="Паспорт" name="passport"/>
                 <Field type="tel" placeholder="Телефон" name="phone"/>
                 <Field type="email" placeholder="Email" name="email"/>
                 <Field type="text" placholder="Дата свадьбы" name="weddingDate"/>
+                <ErrorMessage name="weddingDate" />
                 <Field type="checkbox" placholder="Гражданин Украины ?" name="citizenOfUkraine"/>
                 <Field type="checkbox" placholder="Были ранее женаты ?" name="wasPreviouslyMarried"/>
                 <Field type="text" placholder="Дата рождения" name="dateOfBirth"/>
