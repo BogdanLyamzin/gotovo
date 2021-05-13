282ab89d75ba295a836753583ac603f6845599fe
yaroslav-kulpan <51779177+yaroslav-kulpan@users.noreply.github.com>
2021-05-13 03:14:30 +0300

    +++

--- components/ProfileForm/profile-form.schema.js
+++ components/ProfileForm/profile-form.schema.js
@@ -2,8 +2,20 @@ import * as Yup from "yup";
 
 const profileFormSchema = Yup.object().shape({
     email: Yup.string()
-        .required("Поле обязательное!")
         .email("Введите валидный email!"),
+    fullNameOfTheHusband: Yup.string().required('Поле обязательное!'),
+    WifeSFullName: Yup.string().required('Поле обязательное!'),
+    issuedBy: Yup.string().notRequired(),
+    passport: Yup.string().notRequired(),
+    status: Yup.string().notRequired(),
+    phone: Yup.string().notRequired(),
+    weddingDate: Yup.string().required('Поле обязательное!'),
+    doc: Yup.string().notRequired(),
+    citizenOfUkraine: Yup.bool.notRequired(),
+    wasPreviouslyMarried: Yup.bool.notRequired(),
+    dateOfBirth: Yup.string().notRequired(),
+    plan: Yup.string().notRequired(),
+    comment: Yup.string().notRequired(),
 });
 
 export default profileFormSchema;
