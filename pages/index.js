1451062b4b5d4d28b9c883e21d17ecb065d59481
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-06-30 19:20:28 +0300

    small fix

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,13 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-// import Preview from "../components/common/Preview";
 import Layout from "../components/common/Layout";
-import MainPage from "../components/MainPage";
 
 function Home() {
-  const router = useRouter()
   const { t } = useTranslation('main')
   return (
       <Layout title={t('title')}>
-        <MainPage />
+        {/*<MainPage />*/}
       </Layout>
   )
 }
