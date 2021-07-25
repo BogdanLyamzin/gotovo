4557f3297f675d2d35a933bab504c98e471004a7
yaroslav-kulpan <51779177+yaroslav-kulpan@users.noreply.github.com>
2021-07-25 11:05:03 +0300

    fix not Found

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -24,7 +24,7 @@ const Profile = () => {
 };
 
 export const getServerSideProps = async ({ locale }) => ({
-  fallback: false,
+  notFound: false,
   props: {
     ...(await serverSideTranslations(locale, ["navbar", "footer", "main"])),
   },
